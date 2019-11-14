669ac84ddd88691f2fa5216bfde0557bf2949f8d
Tandi Samples <tandisamples@gmail.com>
2019-11-14 22:19:13 +0000

    first commit

---
+++ lib/bamboo_plants.rb
@@ -0,0 +1,6 @@
+require "bamboo_plants/version"
+
+module BambooPlants
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bamboo_plants/version.rb
@@ -0,0 +1,3 @@
+module BambooPlants
+  VERSION = "0.1.0"
+end
