ab7d7c9a25a2582f97f14cbed584cde72d859945
Tandi Samples <tandisamples@gmail.com>
2019-11-15 01:13:34 +0000

    staus cli stub

--- lib/bamboo_plants/cli.rb
+++ lib/bamboo_plants/cli.rb
@@ -1,6 +1,22 @@
 class BambooPlants::CLI 
   def call 
     puts 'Welcome to Lewis Bamboo!'
-    binding.pry 
+    get_advertised_categories
+    get_bamboo_category
+    # get_bamboo_type
+    # list_plants
+  end
+  
+  def get_advertised_categories
+    # to be scraped instead
+    @category = ['Screening Bamboo', 'Cold Hardy Bamboo', 'Giant Bamboo', 'Indoor and Container Bamboo', 'Clumping Bamboo', 'Staff Picks', 'Large Wholesale Sizes (Frieght Shipping)']
+  end
+  
+  
+    def list_plants
+      # list the categories 
+      @category.each.with_index(1) do |category, index|
+      puts "#{index}. #{category}"
+    end
   end
 end
