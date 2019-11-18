8ca48ce549366c67f6da6c3decbb947859cd556f
Tandi Samples <tandisamples@gmail.com>
2019-11-18 09:20:37 +0000

    add chosen category

--- lib/bamboo_plants/cli.rb
+++ lib/bamboo_plants/cli.rb
@@ -4,28 +4,48 @@ class BambooPlants::CLI
   def call 
     
     puts "\nWelcome to Lewis Bamboo!\n".red.bold
-    puts "Here's our bamboo nursery, which includes the bamboo catagories & number of types within each. "
+    puts "Check out our list of our bamboo plants with the number of choices within each category."
     get_categories
     list_bamboo_categories
+    chosen_category
     categories = BambooPlants::Category.all
-    puts "\nChoose which category you would like to see:".on_blue.bold
     
-    #list categories
-    
-    
+
     end
     
     #respond to input
-    def get_categories
+  def get_categories
     url = "https://lewisbamboo.com/category/bamboo-plants/"
     BambooPlants::Scraper.scrape_categories(url)
     @category = BambooPlants::Category.all
-    end
+    end  
   
   def list_bamboo_categories
     @category.each.with_index(1) do |category, index|
       puts "#{index}. #{category.name}"
   end
+  
+  def chosen_category
+    puts "\nChoose which category you would like to see:".on_blue.bold
+    input = gets.strip.to_i  # represents a number
+      max_value = BambooPlants::Category.all.length  # max makes it more flexible when using the method for other list
+      
+   #   if input.between?(1,max_value) #valid input
+      
+  #      category = @category.all[input - 1] 
+        
+  #      display_categories_items(category)
+  #  else
+        #not valid input
+  #      puts "\nPlease put in a valid category number:".red.bold
+  #      get_categories
+  #      chosen_category
+ #     end
+
+  end
+  
+  
+    
 end  
   
   
